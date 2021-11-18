bf764f818fd1ca5fb836df41e9f9b44b52e94c26
DabooUA <sbardysh@tutanota.com>
2021-11-17 20:22:56 -0500

    adding about us link

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,10 @@
 class StaticController < ApplicationController
-  skip_before_action :verified_user, only: [:home]
+  skip_before_action :verified_user, only: [:home, :about]
 
   def home
+  end
+
+  def about
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   get '/signin', to: 'session#new', as: 'signin'
   post '/session', to: 'session#create', as: 'session'
   delete '/session', to: 'session#destroy'
+  get '/static/about', to: 'static#about'
 end
